8ba62b8a55f18a5ffb421b18bb1119de5c7ae2b1
domlloyd <domlloyd@gmail.com>
2015-07-20 23:05:03 +0100

    Changes made to clean up column names

--- run_analysis.R
+++ run_analysis.R
@@ -10,12 +10,21 @@ setwd ("c:\\users\\dlloyd\\desktop\\UCIHARDataset")
 # first we will read the features.txt file in order to get the column number
 # and column name. This will be required when we need column names for our tidy data set
 # store the result in the variable variable_list
-variable_list <- read.csv("features.txt",header=FALSE,sep=" ",col.names=c('column_num','column_desc'))
+variable_list <- read.csv("features.txt",header=FALSE,sep=" ",col.names=c('column_num','column_desc'),
+                          stringsAsFactors = FALSE)
+# tidy up unusual characters in the column names by replacing with _
+colnames <- gsub('[()--,]','_',variable_list$column_desc)
 
 # read the test and train data sets and store the results in 2 variables
 # testset and trainset respectively
-trainset <- read_table("train\\X_train.txt",col_names = FALSE)
-testset <- read_table("test\\X_test.txt",col_names = FALSE)
+trainset <- read_table("train\\X_train.txt",col_names = colnames)
+testset <- read_table("test\\X_test.txt",col_names = colnames)
 
 # merge the two data sets using rbind and store the result in mergedset
 mergedset <- rbind(trainset,testset)
+
+# select on std and mean columns as requested by the assignment
+# select()
+
+
+# create the output file of tidy data
